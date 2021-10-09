45583e509273da33e05a8d0e839ee596f7840e30
Parul Kumar <prlkumar96@gmail.com>
2021-10-10 02:20:03 +0530

    add bottom bar and fix some bugs

--- script.js
+++ script.js
@@ -15,36 +15,95 @@ let uniqueId = function () {
 
 let tasks = [{
     description: 'Buy some coffee',
-    status: 'active',
     isChecked: false,
     id: uniqueId()
 },
 {
     description: 'Buy milk',
-    status: 'completed',
-    isChecked: false,
-    id: uniqueId()
+    isChecked: true,
+    id: 163380157084
 }
 ];
 
-function listTasks(filterParam) {
-    let html = '';
+let count = 0;
+function setCount() {
+    count = 0;
     for (let i = 0; i < tasks.length; i++) {
-        if (filterParam == tasks[i].status || filterParam == 'all' || filterParam == undefined) {
-            html += `<div class="task-card" id="${tasks[i].id}">
-            <div class="circle"></div>
-            <p class="task-desc">${tasks[i].description}</p>
-            <button onclick="deleteTask(${tasks[i].id})"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18">
-              <path
-                fill="#494C6B"
-                fill-rule="evenodd"
-                d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"
-              />
-            </svg></button>
-          </div>`
+        if (!tasks[i].isChecked) {
+            count++;
         }
     }
+}
+
+let activeFilter = 0;
+
+function bottombar() {
+    let html = '';
+    html += `<div class="bottom-bar"><div>${count} tasks left</div>`;
+    if (activeFilter == 0) {
+        // show bottom bar with 'All' highlighted
+        html += `<div><span class="highlight" onclick="setFilter(0)" >All</span><span onclick="setFilter(1)" >Active</span><span onclick="setFilter(2)">Completed</span></div>`;
+    } else if (activeFilter == 1) {
+        //show bottom bar with active highlighted
+        html += `<div><span onclick="setFilter(0)" >All</span><span class="highlight" onclick="setFilter(1)">Active</span><span onclick="setFilter(2)">Completed</span></div>`;
+    } else {
+        // show completed highlighted
+        html += `<div><span onclick="setFilter(0)" >All</span><span onclick="setFilter(1)">Active</span><span class="highlight" onclick="setFilter(2)">Completed</span></div>`;
+    }
+    html += `<span onclick="clearAllCompleted()">Clear All completed</span></div>`;
+    document.getElementById('task-view').innerHTML += html;
+}
+
+function setFilter(filter) {
+    activeFilter = filter;
+    listTasks();
+}
+
+function clearAllCompleted() {
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].isChecked) {
+            tasks.splice(i, 1);
+        }
+    }
+    listTasks();
+}
+
+function listTasks() {
+    let html = '';
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].isChecked) {
+            if (activeFilter != 1) {
+                html += `<div class="task-card draggable" id="${tasks[i].id}" draggable="true">
+                    <div class="circle check checked" onclick="check(${tasks[i].id})"><svg xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#FFF" stroke-width="2" d="M1 4.304L3.696 7l6-6"/></svg></div>
+                    <p class="task-desc"><strike>${tasks[i].description}</strike></p>
+                    <button onclick="deleteTask(${tasks[i].id})"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18">
+                    <path
+                        fill="#494C6B"
+                        fill-rule="evenodd"
+                        d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"
+                    />
+                    </svg></button>
+                </div>`;
+            }
+        } else {
+            if (activeFilter != 2) {
+                html += `<div class="task-card draggable" id="${tasks[i].id}" draggable="true">
+                    <div class="circle check" onclick="check(${tasks[i].id})"><div class="inner-circle"></div></div>
+                    <p class="task-desc">${tasks[i].description}</p>
+                    <button onclick="deleteTask(${tasks[i].id})"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18">
+                    <path
+                        fill="#494C6B"
+                        fill-rule="evenodd"
+                        d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"
+                    />
+                    </svg></button>
+                    </div>`;
+                }
+            }
+    }
     document.getElementById('task-view').innerHTML = html;
+    setCount();
+    bottombar();
 }
 
 function deleteTask(id) {
@@ -54,7 +113,8 @@ function deleteTask(id) {
         }
     }
     console.log('deleting task with id ' + id);
-    document.getElementById(id).remove();
+    setCount();
+    listTasks();
 }
 
 
@@ -62,22 +122,43 @@ function createNewTask(e) {
     if (e.keyCode == 13) {
         e.preventDefault();
         if (e.target.value == '') return;
-        let obj = { description: e.target.value, status: 'active', isChecked: false, id: uniqueId() };
+        let obj = { description: e.target.value, isChecked: false, id: uniqueId() };
         tasks.push(obj);
         listTasks();
-        console.log('last child(svg) ' + obj.id.lastChild);
-       
         e.target.value = '';
     }
 }
 
+function check(cardId) {
+    count = 0;
+    for (let i = 0; i < tasks.length; i++) {
+        if (cardId == tasks[i].id) {
+            tasks[i].isChecked = !tasks[i].isChecked;
+        }
+    }
+    listTasks();
+}
+
+
 listTasks();
-// for (let i = 0; i < tasks.length; i++) {
-//     let id = tasks[i].id;
-//     console.log('adding listener' + tasks[i].id);
-//     document.getElementById(id).lastChild.addEventListener('click', deleteTask);
-// }
 
 document.getElementById('new-task').addEventListener("keyup", createNewTask);
+
+// const draggables = document.querySelectorAll('.draggable');
+// const constainer = document.querySelector('task-view');
+
+// draggables.forEach(draggable => {
+//     draggable.addEventListener('dragstart', () => {
+//         console.log('dragging');
+//         draggable.classList.add('dragging');
+//     });
+//     draggable.addEventListener('dragend', () => {
+//         draggable.classList.remove('dragging');
+//     })
+// });
+
+// function getDragAfterElement(task, y) {
+//     const draggableElements = [...document.querySelectorAll('draggable:not(.dragging)')]
+// }
 
 
